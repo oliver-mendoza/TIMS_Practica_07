36820fee6264361addad2d1fb8d3b43295ffc34d
oliver-mendoza <oliver.mendoza@estudiante.ucsm.edu.pe>
2025-04-19 17:37:35 -0500

    test: agregar lab_05 con pruebas de la clase Autenticador

---
+++ lab_05/src/actividad_01/Autenticador.java
@@ -0,0 +1,23 @@
+package actividad_01;
+public class Autenticador {
+    private String[] usuariosPermitidos = {"admin","usuario", "invitado"};
+    public boolean autenticar(String usuario, String password) {
+        if(usuario == null || password == null){
+            return false;
+        }
+        if(password.length() < 4){
+            return false;
+        }
+        boolean usuarioValido = false;
+        for(String permitido : usuariosPermitidos){
+            if(permitido.equals(usuario)){
+                usuarioValido = true;
+                break;
+            }
+        }
+        if(!usuarioValido){
+            return false;
+        }
+        return password.equals("1234");
+    }
+}
